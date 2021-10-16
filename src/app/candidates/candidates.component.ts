934eae645d52054a85f112f4e3ab17eb0c0925d4
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-10-17 00:22:00 +0300

    added filters

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Candidate {
-  id: number
-  firstName: string
-  lastName: string
-  isActive: boolean
-  // email: string
-  // skype: string
-  // phone: string
-  // specialisation: string
-  // location: string
-  // englishLevel: string
-}
+// export interface Candidate {
+//   id: number
+//   firstName: string
+//   lastName: string
+//   isActive: boolean
+//   email: string
+//   skype: string
+//   phone: string
+//   specialisation: string
+//   location: string
+//   englishLevel: string
+//   solution: string
+// }
 
 @Component({
   selector: 'app-candidates',
@@ -20,23 +21,45 @@ export interface Candidate {
 })
 export class CandidatesComponent implements OnInit {
 
-  candidates: Candidate[] = [
-    {id: 1, firstName: 'John', lastName: 'Smith', isActive: false},
-    {id: 2, firstName: 'Anna', lastName: 'Donovan', isActive: false},
-    {id: 3, firstName: 'Rick', lastName: 'Gonsales', isActive: false},
-    {id: 4, firstName: 'Mark', lastName: 'Tornhes', isActive: false},
-    {id: 5, firstName: 'Britney', lastName: 'Sterry', isActive: false},
-    {id: 6, firstName: 'George', lastName: 'McTanney', isActive: false},
+  candidates: any[] = [
+    {id: 1, firstName: 'John', lastName: 'Smith', isActive: false, solution: 'accepted'},
+    {id: 2, firstName: 'Anna', lastName: 'Donovan', isActive: false, solution: 'declined'},
+    {id: 3, firstName: 'Rick', lastName: 'Gonsales', isActive: false, solution: 'declined'},
+    {id: 4, firstName: 'Mark', lastName: 'Tornhes', isActive: false, solution: 'accepted'},
+    {id: 5, firstName: 'Britney', lastName: 'Sterry', isActive: false, solution: 'questionable'},
+    {id: 6, firstName: 'George', lastName: 'McTanney', isActive: false, solution: 'declined'},
+    {id: 7, firstName: 'Bich', lastName: 'Bichovich', isActive: false, solution: 'declined'},
+    {id: 8, firstName: 'Morgan', lastName: 'Freeman', isActive: false, solution: 'questionable'},
+    {id: 9, firstName: 'Lucas', lastName: 'Tounsend', isActive: false, solution: 'accepted'},
+    {id: 10, firstName: 'Charles', lastName: 'Brighton', isActive: false, solution: 'declined'},
   ]
+
+  resultedArray: any[] = this.candidates
+
+  activeFilter: string = 'all'
+
+  filters: string[] = ['all', 'accepted', 'questionable', 'declined']
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   selectCandidate(id: number) {
-    this.candidates.forEach(candidate => candidate.isActive = false);
-    const index = this.candidates.findIndex(candidate => candidate.id === id);
-    this.candidates[index].isActive = true;
+    this.resultedArray.forEach(candidate => candidate.isActive = false);
+    const index = this.resultedArray.findIndex(candidate => candidate.id === id);
+    this.resultedArray[index].isActive = true;
+  }
+
+  filterCandidates(filter: string) {
+    this.activeFilter = filter
+    console.log(this.activeFilter)
+    this.resultedArray.forEach(candidate => candidate.isActive = false)
+    if (filter === 'all') {
+      this.resultedArray = this.candidates
+    } else {
+      this.resultedArray = this.candidates.filter(candidate => candidate.solution === filter)
+    }
   }
 
 }
