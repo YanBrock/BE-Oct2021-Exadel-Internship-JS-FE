6f015fad8b1794de6fd8477c243543b9bcb40c2f
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-10-21 17:43:14 +0300

    some changes in types and services

--- src/app/types/candidate.ts
+++ src/app/types/candidate.ts
@@ -8,13 +8,7 @@ export interface Candidate {
   id: number
   firstName: string
   lastName: string
-  isActive: boolean
-  // email: string
-  // skype: string
-  // phone: string
   specialisation: string
-  // location: string
-  // englishLevel: string
   isInterviewedByHr?: boolean
   isInterviewedByTech?: boolean
   status?: Status
