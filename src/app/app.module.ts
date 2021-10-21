15ed6adf66d1fe4663a78bae185f9291af3e1daf
Yan_Brock <waitmeforcrime@gmail.com>
2021-10-21 21:52:04 +0300

    changed structure of master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CandidateFormComponent } from './pages/candidate-form/candidate-form.component';
+import { UserLoginComponent } from './pages/user-login/user-login.component';
+import { RecruiterComponent } from './pages/recruiter/recruiter.component';
+import { TechInterviewerComponent } from './pages/tech-interviewer/tech-interviewer.component';
+import { MentorComponent } from './pages/mentor/mentor.component';
+import { ManagerComponent } from './pages/manager/manager.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CandidateListComponent } from './components/candidate-list/candidate-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    MainComponent,
+	FooterComponent,
+	CandidateFormComponent,
+	UserLoginComponent,
+	RecruiterComponent,
+	TechInterviewerComponent,
+	MentorComponent,
+	ManagerComponent,
+	AdminComponent,
+	CalendarComponent,
+	CandidateListComponent,
   ],
   imports: [
     BrowserModule,
