dc5d6ce23a626cac93619a5b7e94ddd8317cd156
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-10-23 00:22:35 +0300

    created structure for recruiter component

---
+++ src/app/pages/recruiter/recruiter-window/recruiter-window.component.ts
@@ -0,0 +1,20 @@
+import { Candidate } from './../../../types/candidate';
+import { CandidatesService } from 'src/app/services/candidates.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruiter-window',
+  templateUrl: './recruiter-window.component.html',
+  styleUrls: ['./recruiter-window.component.scss']
+})
+export class RecruiterWindowComponent implements OnInit {
+
+  selectedCandidate: Candidate
+
+  constructor(private candidatesService: CandidatesService) { }
+
+  ngOnInit(): void {
+    this.candidatesService.selectedCandidate.subscribe(candidate => this.selectedCandidate = candidate)
+  }
+
+}
