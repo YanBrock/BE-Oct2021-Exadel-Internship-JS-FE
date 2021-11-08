9fe4ba6d719f4424ccd96ae72de56aa9a236c77e
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-11-08 20:33:40 +0300

    creating module and some fixes

--- src/app/pages/tech-interviewer/tech-interviewer.module.ts
+++ src/app/pages/tech-interviewer/tech-interviewer.module.ts
@@ -1,3 +1,4 @@
+import { CandidateDataModule } from './../../components/candidate-data/candidate-data.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TechInterviewerComponent } from './tech-interviewer.component';
@@ -8,20 +9,16 @@ import { CandidatesService } from 'src/app/services/candidates.service';
 import { InterviewerAssesmentComponent } from './interviewer-assesment/interviewer-assesment.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { TechInterviewerWindowComponent } from './tech-interviewer-window/tech-interviewer-window.component';
-import { CandidateDataComponent } from './candidate-data/candidate-data.component';
 import { RecruiterAssesmentComponent } from './recruiter-assesment/recruiter-assesment.component'
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-// import { FilterByStatusPipe } from 'src/app/pipes/filter-by-status.pipe';
 
 @NgModule({
   declarations: [
     TechInterviewerComponent,
     InterviewerAssesmentComponent,
     TechInterviewerWindowComponent,
-    CandidateDataComponent,
     RecruiterAssesmentComponent,
-    // FilterByStatusPipe
   ],
   imports: [
     FormsModule,
@@ -30,7 +27,8 @@ import { FormsModule } from '@angular/forms';
     MatListModule,
     MatInputModule,
     TranslateModule,
-    MatButtonModule
+    MatButtonModule,
+    CandidateDataModule
   ],
   exports: [
     TechInterviewerComponent
