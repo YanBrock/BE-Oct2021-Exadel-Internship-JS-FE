a212a015a0e0788166a5c13078dca1cff3514a75
Andrey-Mkoyan <and.mkoyan07@gmail.com>
2021-11-11 17:01:43 +0400

    add mentor component

---
+++ src/app/pages/mentor/mentor-review/mentor-review.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mentor-review',
+  templateUrl: './mentor-review.component.html',
+  styleUrls: ['./mentor-review.component.scss']
+})
+export class MentorReviewComponent implements OnInit {
+  scores = {
+    grade: '',
+    review: ''
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  OnlyNumbersAllowed(event): boolean {
+    if (this.scores.grade.length > 0 && this.scores.grade[0] !== '1') return false
+    if (this.scores.grade.length > 0 && event.key !== '0') {
+      return false
+    }
+    return /^([0-9]|10)$/.test(event.key) && this.scores.grade.length <= 1
+  }
+
+  onSubmit() {
+    console.log(this.scores)
+    this.scores = {
+      grade: '',
+      review: ''
+    }
+  }
+
+}
