46b127f6865839815df840ea1fe3baa0ebebebb1
iskonplus <58971909+iskonplus@users.noreply.github.com>
2021-11-11 15:48:46 +0300

    delete comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,15 +13,13 @@ import { UserGuard } from './guards/user.guard';
 
 
 const routes: Routes = [
-	// { path: '', component: UserLoginComponent},
-	{ path: 'zz', component: UserLoginComponent},
+	{ path: '', component: UserLoginComponent},
 	{ path: 'form', component: FormComponent},
 	{ path: 'recruiter', component: RecruiterComponent, canActivate: [UserGuard]},
 	{ path: 'tech-interviewer', component: TechInterviewerComponent, canActivate: [UserGuard]},
 	{ path: 'mentor', component: MentorComponent, canActivate: [UserGuard]},
 	{ path: 'manager', component: ManagerComponent, canActivate: [UserGuard]},
-	// { path: 'admin', component: AdminComponent, canActivate: [UserGuard]},
-	{ path: '', component: AdminComponent},
+	{ path: 'admin', component: AdminComponent, canActivate: [UserGuard]},
 	{ path: '**', component: NotFoundComponent},
 ];
 
