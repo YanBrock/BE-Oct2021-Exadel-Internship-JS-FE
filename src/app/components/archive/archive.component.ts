f7952c1f65f0cf121824a5b0b5b239d4b2084c59
Yan_Brock <waitmeforcrime@gmail.com>
2021-11-28 12:56:15 +0300

    added mock archive btn

--- src/app/components/archive/archive.component.ts
+++ src/app/components/archive/archive.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CandidatesService } from 'src/app/services/candidates.service';
+import { loadCandidatesList } from 'src/app/store/candidates/actions';
+import { Candidate } from 'src/app/types/candidate';
 
 @Component({
-  selector: 'app-archive',
-  templateUrl: './archive.component.html',
-  styleUrls: ['./archive.component.scss']
+	selector: 'app-archive',
+	templateUrl: './archive.component.html',
+	styleUrls: ['./archive.component.scss']
 })
 export class ArchiveComponent implements OnInit {
 
-  constructor() { }
+	allCandidates: any;
 
-  ngOnInit(): void {
-  }
+	constructor(private store: Store, private candidatesService: CandidatesService) { }
+
+	toArchive() {
+		this.store.select(loadCandidatesList).subscribe(data => this.allCandidates = data);
+		console.log(this.allCandidates);
+	}
+
+	ngOnInit(): void {
+	}
 
 }
