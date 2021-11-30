be7be2c1de694ec5fd572c02a92dd9869d0fe596
iskonplus <58971909+iskonplus@users.noreply.github.com>
2021-11-30 10:55:02 +0300

    fixed header styles

--- src/app/components/admin-nav/admin-nav.component.ts
+++ src/app/components/admin-nav/admin-nav.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/services/admin-service';
+import { UserLoginService } from 'src/app/services/user-login.service';
 
 @Component({
-	selector: 'app-admin-nav',
-	templateUrl: './admin-nav.component.html',
-	styleUrls: ['./admin-nav.component.scss']
+  selector: 'app-admin-nav',
+  templateUrl: './admin-nav.component.html',
+  styleUrls: ['./admin-nav.component.scss']
 })
 export class AdminNavComponent implements OnInit {
 
-	selectedOption: string = "Home";
-	displayComponent: string = "admin"
-	
+  selectedOption = "Home";
+  displayComponent = "admin";
 
-	constructor(private adminService: AdminService) { }
 
-	changeWindow() {
-		this.adminService.setSelectedPage(this.selectedOption);
-	};
+  constructor(private adminService: AdminService, private userLoginService: UserLoginService) {
+    // this.displayComponent = this.userLoginService.getRole();
+    this.displayComponent = this.userLoginService.activeUser.role;
+    // this.userLoginService.userRole$.subscribe(role => {
+    //   console.log(role)
+    //   this.displayComponent = role;
+    // });
+  }
 
-	ngOnInit(): void {
-	};
+
+  changeWindow() {
+    this.adminService.setSelectedPage(this.selectedOption);
+  };
+
+
+  ngOnInit(): void {};
+
+
 }
